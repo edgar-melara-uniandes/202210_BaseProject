46ed15dea9a94c661826516f333ecc1522ac1a1c
edgar-melara-uniandes <e.melara@uniandes.edu.co>
2022-05-01 20:03:09 -0600

    feat: agregar listado de suma de tipos de planta

--- src/app/plantas/lista-plantas/lista-plantas.component.ts
+++ src/app/plantas/lista-plantas/lista-plantas.component.ts
@@ -9,14 +9,26 @@ import { Planta } from '../planta';
 })
 export class ListaPlantasComponent implements OnInit {
   plantas: Array<Planta> = [];
+  sumaTipos: Array<number> = [];
   constructor(private plantasService: PlantasService) { }
   getPlantas (): void{
     this.plantasService.getPlantas().subscribe((plantas)=>{
       this.plantas = plantas;
     })
   }
+
+  sumTipos(): void{
+
+    this.plantasService.getPlantas().subscribe((plantas)=>{
+      const interior  = plantas.filter((val)=> val.tipo === 'Interior').length
+      const exterior  = plantas.filter((val)=> val.tipo === 'Exterior').length
+      this.sumaTipos = [interior, exterior]
+    })
+
+  }
   ngOnInit() {
     this.getPlantas();
+    this.sumTipos();
   }
 
 }
